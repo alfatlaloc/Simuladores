5e28e3dbb8606e7b8144f55e316a46e9d0acbb41
alfatlaloc <mr.alfatlaloc@gmail.com>
2021-12-21 19:52:04 -0600

    Practica

--- src/Components/HomeFiles/Practica.tsx
+++ src/Components/HomeFiles/Practica.tsx
@@ -1,12 +1,18 @@
+import { Button } from "react-bootstrap";
+import pdf from "../../docs/practica.pdf";
 const Practica = () => {
   return (
-    <section className="textosEncuadre">
+    <section className="pagina">
       <h1>Práctica</h1>
       <p>
         Después de revisar el contenido temático y utilizando este simulador del
         punto de equilibrio, realiza la siguiente práctica:
       </p>
-      <a href={require("../../docs/practica.pdf")} target="_blank" > Descargar</a>
+      <Button type="button" className="m-1 buttonPrimary">
+        <a className="decoNone" href={pdf} download="practica.pdf" target="_blank">
+          Descargar
+        </a>
+      </Button>
     </section>
   );
 };
