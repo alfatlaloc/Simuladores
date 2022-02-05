de42182844c3415a656e3912cf1829dac263ad95
alfatlaloc <mr.alfatlaloc@gmail.com>
2022-02-04 21:10:26 -0600

    footer creative commons

--- src/Components/Common/footer/Footer.tsx
+++ src/Components/Common/footer/Footer.tsx
@@ -8,50 +8,37 @@ const Footer = () => {
 
         <div className="container text-center text-md-start mt-5">
           <div className="row mt-3">
-            <div className="col-md-4 col-lg-3 col-xl-6 mx-auto mb-4">
+            <div className="col-lg-3 col-xl-6 mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Contactos</h6>
               <p>
-                <strong>Marco Antonio Castro Gutiérrez</strong>
+                <strong>Josefina Hernández Jaime</strong>{"   "}|
+                <strong> Correo: </strong> johernandezja@ipn.mx
               </p>
               <p>
-                <strong>Github: </strong>
-                <a
-                  className="githubLink"
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://github.com/alfatlaloc"
-                >
-                  https://github.com/alfatlaloc
-                </a>
+                <strong>Eduardo Rodríguez Flores</strong>{"   "}|
+                <strong> Correo: </strong> erodrigrezf@ipn.mx
               </p>
+
               <p>
-                <strong>Correo: </strong> macg.ae1313@gmail.com
+                <strong>Yasmin Ivette Jiménez Galán</strong>{"   "}|
+                <strong> Correo: </strong> yjimenezg@ipn.mx
               </p>
-              <p>
-                <strong>Arturo Morgado Jiménez</strong>
-              </p>
-              <p>
-                <strong>Github: </strong>
-                <a
-                  className="githubLink"
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://github.com/LordMorgado"
-                >
-                  https://github.com/LordMorgado
-                </a>
-              </p>
-              <p>
-                <strong>Correo: </strong> arturomorgado117@gmail.com
-              </p>
+            </div>
+            <div className="col-lg-3 col-xl-6 mx-auto mb-4">
+              <strong>
+                <div
+                  className="license"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">Herramienta digital para el cálculo del punto de equilibrio operativo</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://simuladorpuntodeequilibrio.herokuapp.com/" property="cc:attributionName" rel="cc:attributionURL">Josefina Hernández Jaime, Eduardo Rodríguez Flores & Yasmín Ivette Jiménez Galán</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional License</a>.<br />Creado a partir de la obra en <a xmlns:dct="http://purl.org/dc/terms/" href="https://simuladorpuntodeequilibrio.herokuapp.com/" rel="dct:source">https://simuladorpuntodeequilibrio.herokuapp.com/</a>.
+    `,
+                  }}
+                />
+              </strong>
             </div>
           </div>
         </div>
       </section>
-
-      <div className="text-center p-4">
-        © {new Date().getFullYear()} Copyright: Simulador financiero
-      </div>
     </footer>
   );
 };
